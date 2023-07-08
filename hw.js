5dfddd60c4c6ce3bb0ddd0956557aee5a1a96b75
qwertyNippon <orlandohenry100@gmail.com>
2023-07-08 15:14:32 -0400

    way too long

--- hw.js
+++ hw.js
@@ -1,3 +1,75 @@
+
+
+//==================Exercise #1 ==========//
+/*Write a function that takes in the string and the list of dog names, loops through 
+the list and checks that the current name is in the string passed in. The output should be:
+"Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
+*/
+
+let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
+let dog_names = ["Max","HAS","PuRple","dog"]
+
+function findWords(dog_string, dog_name){
+    d_s = dog_string;
+    d_n = dog_name;
+    // for (let i in d_n){
+        
+    // }
+    for (let i = 0; i < d_n.length; i++) {
+        console.log(d_n[0]);
+        if (d_s.includes(d_n[i])){
+            console.log('matched' + d_n[i]);
+        return console.log('no matches');
+        }
+      }
+}
+
+
+
+findWords(dog_string, dog_names)
+
+
+
+// function countByOne(){
+//     // For Loop
+//     for(let i = 0; i < 20; i++){
+//         console.log(i)
+//     }
+//     return 'Counting has stopped'
+// }
+
+//Call method here with parameters
+
+//============Exercise #2 ============//
+/*Write a fucntion that takes in an array and removes every even index with a splice,
+and replaces it with the string "even index" */
+
+Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+
+function replaceEvens(arr){
+    //code goes here
+}
+
+//Expected output
+//Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+//Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Two codewars are below
+
+
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 // For example, for [1, 2, 2] it should return 9 because 
